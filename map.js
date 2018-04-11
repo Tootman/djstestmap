5fdfecfda53adc77cf6b138d1fec558c921fd792
Dan Simmons <danieljsimmons@btinternet.com>
2018-04-11 19:14:59 +0100

    added geoJSON and own namespace

--- map.js
+++ map.js
@@ -1,60 +1,117 @@
-var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
-var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w';
+"use strict";
 
-var greyscale = L.tileLayer(mbUrl, { id: 'mapbox.light', attribution: mbAttr, maxZoom:22 });
-var streets = L.tileLayer(mbUrl, { id: 'mapbox.streets', attribution: mbAttr, maxZoom:22 });
-var layer = L.tileLayer(mbUrl, { id: 'mapbox.satellite', attribution: mbAttr, maxZoom:22 });
+var App = {}; // container for map vars
+App.mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+App.mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w';
 
-var myLayerGroup = L.layerGroup();
+App.greyscaleLayer = L.tileLayer(App.mbUrl, { id: 'mapbox.light', attribution: App.mbAttr, maxZoom: 22 });
+App.streetsLayer = L.tileLayer(App.mbUrl, { id: 'mapbox.streets', attribution: App.mbAttr, maxZoom: 22 });
+App.satLayer = L.tileLayer(App.mbUrl, { id: 'mapbox.satellite', attribution: App.mbAttr, maxZoom: 22 });
 
-var map = L.map('map', {
+App.myLayerGroup = L.layerGroup();
+
+
+// create map with 3 layers
+App.map = L.map('map', {
     center: [51.4379409, -0.3185518],
     zoom: 20,
-    maxZoom:22,
-    layers: [layer, streets, greyscale]
+    maxZoom: 22,
+    layers: [App.satLayer, App.streetsLayer, App.greyscaleLayer]
 });
 
-var baseMaps = {
-    "Greyscale": greyscale,
-    "Streets": streets,
-    "Satallite map": layer
+// group of basemap layers
+App.baseMaps = {
+    "Greyscale": App.greyscaleLayer,
+    "Streets": App.streetsLayer,
+    "Satallite map": App.satLayer
 };
 
-
-var overlayMaps = {
-    "My Features": myLayerGroup
+// group of overlay layers
+App.overlayMaps = {
+    "My Features": App.myLayerGroup
 };
 
+// Add the layers control
+L.control.layers(App.baseMaps, App.overlayMaps).addTo(App.map);
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-var baseMaps = {
-    "<span style='color: gray'>Grayscale</span>": layer
+// -----------------------------
 
-};
+/*
+var myData = loadJSON('ham-green-demo.json',
+         function(data) { console.log(data); },
+         function(xhr) { console.error(xhr); }
+);
+
+console.log("myData: " + myData);
+*/
+
+// -------------------------
+var data = 8;
+console.log("init data val: " + data);
+
+fetch('ham-green-demo.json')
+    .then(
+        function(response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            // access data eg App.geoData.features[5].properties.Asset
+            response.json().then(function(data) {
+                console.log(data);
+                App.geoData = data;
+                console.log("App.myData inside func: " + App.geoData);
+                App.geoLayer = L.geoJson(data, {
+
+                    onEachFeature: function(feature, layer) {
+                        console.log(feature.properties.Asset);
+                        layer.on('click', function() {
+                            whenGeoFeatureClicked(feature);
+                            console.log("clicked: " + feature.properties.Asset);
+                        });
+                    }
+                });
+                App.map.addLayer(App.geoLayer);
+                App.myLayerGroup.addLayer(App.geoLayer);
+                //L.control.addOverlay(layer, name)
+            });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+
+console.log("final data val: " + data);
+console.log("App.myData: " + App.geoData);
+
+
 
 /*
 var map = L.map('map').setView([51.4379409,-0.3185518], 18);
 var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w', {
-	maxZoom: 22,
+    maxZoom: 22,
 
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'mapbox.satellite',
-	accessToken: 'pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w',
-	useCache: true,
-	crossOrigin: true
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w',
+    useCache: true,
+    crossOrigin: true
 });
 
 */
 
 /*
 var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w', {
-	maxZoom: 22,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'mapbox.satellite',
-	accessToken: 'pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w',
-	useCache: true,
-	crossOrigin: true
+    maxZoom: 22,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoiZGFuc2ltbW9ucyIsImEiOiJjamRsc2NieTEwYmxnMnhsN3J5a3FoZ3F1In0.m0ct-AGSmSX2zaCMbXl0-w',
+    useCache: true,
+    crossOrigin: true
 });
 
 */
@@ -62,6 +119,8 @@ var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?ac
 
 // Listen to cache hits and misses and spam the console
 // The cache hits and misses are only from this layer, not from the WMS layer.
+
+/*
 layer.on('tilecachehit', function(ev) {
     console.log('Cache hit: ', ev.url);
 });
@@ -72,9 +131,12 @@ layer.on('tilecacheerror', function(ev) {
     console.log('Cache error: ', ev.tile, ev.error);
 });
 
-layer.addTo(map);
+layer.addTo(App.map);
 
-var wmsLayer = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
+*/
+
+/*
+App.wmsLayer = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
     layers: 'nexrad-n0r-900913',
     format: 'image/png',
     transparent: true,
@@ -85,20 +147,22 @@ var wmsLayer = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nex
     crossOrigin: true
 });
 
-wmsLayer.addTo(map);
+// App.wmsLayer.addTo(map);
+*/
 
-var lc = L.control.locate({
+App.lc = L.control.locate({
     position: 'topright',
     strings: {
         title: "Show me where I am, yo!"
     }
-}).addTo(map);
+}).addTo(App.map);
 
 
 // Seed the base layer, for the whole world, for zoom levels 0 through 4.
+/*
 function seed() {
-    var bbox = L.latLngBounds(L.latLng(-85, -180), L.latLng(85, 180));
-    layer.seed(bbox, 0, 4);
+    bbox = L.latLngBounds(L.latLng(-85, -180), L.latLng(85, 180));
+    layer.seed(App.bbox, 0, 4);
 }
 // Display seed progress on console
 layer.on('seedprogress', function(seedData) {
@@ -108,73 +172,100 @@ layer.on('seedprogress', function(seedData) {
 layer.on('seedend', function(seedData) {
     console.log('Cache seeding complete');
 });
+*/
 
-
-var sidebar = L.control.sidebar('sidebar', {
+// sidebar plugin
+App.sidebar = L.control.sidebar('sidebar', {
     position: 'left',
     closeButton: 'true'
 });
-
-map.addControl(sidebar);
+App.map.addControl(App.sidebar);
 
 // window.onload = function() {
 
-var sidebarMarker = L.marker([51.4379409, -0.3185518], { title: 'Litter Bin' }).addTo(myLayerGroup).on('click', function() {
+App.sidebarMarker = L.marker([51.4379409, -0.3185518], { title: 'Litter Bin' }).addTo(App.myLayerGroup).on('click', function() {
     //sidebar.setContent("Information,  instructions, option to edit or take photo -  everything regarding the " + sidebarMarker.options.title);
 
-    sidebar.setContent(document.getElementById("test-div").innerHTML);
+    App.sidebar.setContent(document.getElementById("test-div").innerHTML);
     // console.log("test-div: " + document.querySelector("test-div"));
 
-    sidebar.toggle();
+    App.sidebar.toggle();
     console.log("clicked marker");
-
 });
 
-var sidebarMarker2 = L.marker([51.4389529, -0.3195528], { title: 'Bench' }).addTo(myLayerGroup).on('click', function() {
-    sidebar.setContent(document.getElementById("test-div").innerHTML);
-    sidebar.toggle();
+App.sidebarMarker2 = L.marker([51.4389529, -0.3195528], { title: 'Bench' }).addTo(App.myLayerGroup).on('click', function() {
+    App.sidebar.setContent(document.getElementById("test-div").innerHTML);
+    App.sidebar.toggle();
     console.log("clicked marker2");
-
+    console.log("App.myData: " + App.myData);
 });
 
-// };
-
-L.Control.watermark  = L.Control.extend({
-    onAdd: function(map) {
-        const watermark = L.DomUtil.create('IMG', 'custom-control');
-        watermark.src = 'ORCL-logo-cropped.png';
-        watermark.style.opacity =0.3;
-        watermark.style.background="none";
+L.Control.watermark = L.Control.extend({
+    onAdd: (e) => {
+        App.watermark = L.DomUtil.create('IMG', 'custom-control');
+        App.watermark.src = 'ORCL-logo-cropped.png';
+        App.watermark.style.opacity = 0.3;
+        App.watermark.style.background = "none";
         // watermark.style.width = '200px';
-        return watermark;
+        return App.watermark;
     },
-    onRemove: function(map) {
+    onRemove: (e) => {
         // Nothing to do here
     }
 });
 L.control.watermark = (opts) => { return new L.Control.watermark(opts) };
-L.control.watermark({ position: 'bottomright' }).addTo(map);
-
-L.control.scale().addTo(map);
-
-
-
+L.control.watermark({ position: 'bottomright' }).addTo(App.map);
+L.control.scale().addTo(App.map);
 
 L.Control.myControl = L.Control.extend({
-    onAdd: function(map) {
+    onAdd: (e) => {
         const myControl_div = L.DomUtil.create('div', 'custom-control');
-         myControl_div.onclick = function(){
-         	console.log("custom control clicked!");
-         	alert ("Load Shapefile or do something else");
-         }
+        myControl_div.onclick = function() {
+            console.log("custom control clicked!");
+            alert("Load Shapefile or do something else");
+        }
         return myControl_div;
     },
-    onRemove: function(map) {
+    onRemove: (e) => {
         // Nothing to do here
     }
 });
 
 L.control.myControl = (opts) => { return new L.Control.myControl(opts) };
-L.control.myControl({ position: 'bottomright' }).addTo(map);
+L.control.myControl({ position: 'bottomright' }).addTo(App.map);
+
+function initGeoLayers() {
+    console.log("initGeoLayers called");
+}
+
+function whenGeoFeatureClicked(feature){
+    App.sidebar.setContent(document.getElementById("test-div").innerHTML);
+    App.sidebar.toggle();
+}
 
 
+// --------------------------------------------- helpers ------
+
+
+
+
+/*
+function loadJSON(path, success, error)
+{
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                     success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+*/
